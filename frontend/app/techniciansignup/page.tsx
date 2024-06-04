33d3fbf2634e9d142a64bbdf95eef0484dc926f4
ante-neh <amareanteneh12@gmail.com>
2024-06-04 15:53:16 +0300

    feat(web):add technician registration page with all functionalities

---
+++ frontend/app/techniciansignup/page.tsx
@@ -0,0 +1,9 @@
+import RegisterTechnician from "@/components/Technician/technicianRegister/register"
+
+const Signup = () => {
+  return (
+    <RegisterTechnician/>
+  )
+}
+
+export default Signup
